abe3f4aa69a23c8d8b640c48e1fee6e500e2d4e9
Thaslima <nizuthasli15@gmail.com>
2023-07-10 10:27:14 +0530

    10commit

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+
+    
+
 window.addEventListener("scroll",function(e){
     if(this.window.scrollY>4)
     {
@@ -10,71 +13,25 @@ header.className="sticky";
 header.className="header";
     }
 })
-// function myfunction()
-// {
-//     console.log("hello")
-//     let navbar=document.querySelector('navbar')
-//     console.log(navbar.className)
-// }
 
-// window.addEventListener("scroll",function(e)
-// {
-//     console.log(this.window.scrollY)
-//     let arr=['HTML','CSS','JAVASCRIPT','MONGODB','REACTJS','MYSQL']
-//   for(let i=0;i<arr.length;i++)
-//   {
-//     move()
-//   }
-//     function move()
-//     {
-//            if(this.window.scrollY>900)
-//     {
-        
-//                     var i = 0;
 
-//   if (i == 0) {
-//     i = 1;
-//     var skill=document.getElementById("skillidcss")
-//     console.log(skill.firstChild.textContent)
-//     var elem = document.getElementById("progressvalue");
-//     console.log(elem)
-//     var width = 1;
-//     var value=document.getElementById("value")
-//     console.log(value.innerHTML)
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//       if (width >= 95) {
-//         clearInterval(id);
-//         i = 0;
-//       } else {
-//         width++;
-//         elem.style.width = width + "%";
-//       }
+//  function sendEmail() {
+//       Email.send({
+//         Host: "smtp.gmail.com",
+//         Username: "sender@email_address.com",
+//         Password: "Enter your password",
+//         To: 'receiver@email_address.com',
+//         From: "sender@email_address.com",
+//         Subject: "Sending Email using javascript",
+//         Body: "Well that was easy!!",
+//       })
+//         .then(function (message) {
+//           alert("mail sent successfully")
+//         });
 //     }
-  
-// }
-// }
 
-//     }
- 
-// })
-        
-
-// var i = 0;
-// function move() {
-//   if (i == 0) {
-//     i = 1;
-//     var elem = document.getElementById("myBar");
-//     var width = 1;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//       if (width >= 90) {
-//         clearInterval(id);
-//         i = 0;
-//       } else {
-//         width++;
-//         elem.style.width = width + "%";
-//       }
-//     }
-//   }
-// }
+function myClose()
+{
+  let display=document.getElementById('navbar')
+  display.style.display="none"
+}
